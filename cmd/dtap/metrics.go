fcd6f878f4dff69158f9a1dae32e18536ff4d2d9
Diego Bendersky <diegobendersky@gmail.com>
2021-11-15 11:51:51 -0300

    remove unused function metricsExporter

--- cmd/dtap/metrics.go
+++ cmd/dtap/metrics.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"net"
-	"time"
-
-	graphite "github.com/cyberdelia/go-metrics-graphite"
 	"github.com/rcrowley/go-metrics"
-	log "github.com/sirupsen/logrus"
 )
 
 var (
@@ -22,13 +17,3 @@ var (
 	// The total number of lost output frames from buffer
 	TotalLostOutputFrame = metrics.GetOrRegisterCounter("dtap_output_lost_frame_total", nil)
 )
-
-func metricsExporter(address string, interval time.Duration) error {
-	addr, err := net.ResolveTCPAddr("tcp", address)
-	if err != nil {
-		return err
-	}
-	go graphite.Graphite(metrics.DefaultRegistry, interval, "metrics", addr)
-	log.Info("start metrics exporter")
-	return nil
-}
